297902730d39fa84693613b7990034f12054d717
cchrissmm <chris@escapeecampers.com>
2024-01-03 18:16:55 +1100

    calibrated

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@
 
 HX711 scale;
 
-float calibration_factor = -28050; //-7050 worked for my 440lb max scale setup
+float calibration_factor = -34200; //-7050 worked for my 440lb max scale setup
 
 void setup() {
   Serial.begin(115200);
@@ -63,7 +63,7 @@ void loop() {
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
   Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1);
+  Serial.print(scale.get_units(), 3);
   Serial.print(" kg"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
   Serial.print(" calibration_factor: ");
   Serial.print(calibration_factor);
