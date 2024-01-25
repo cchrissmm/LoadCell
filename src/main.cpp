a79f7a1658bdbee3b3cd849c6ad3e93ded46e3c3
cchrissmm <51454310+cchrissmm@users.noreply.github.com>
2024-01-25 18:17:22 +1100

    running load and gps at 30Hz

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,3 @@
-/*
- Example using the SparkFun HX711 breakout board with a scale
- By: Nathan Seidle
- SparkFun Electronics
- Date: November 19th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
- This is the calibration sketch. Use it to determine the calibration_factor that the main example uses. It also
- outputs the zero_factor useful for projects that have a permanent mass on the scale in between power cycles.
-
- Setup your scale and start the sketch WITHOUT a weight on the scale
- Once readings are displayed place the weight on the scale
- Press +/- or a/z to adjust the calibration_factor until the output readings match the known weight
- Use this calibration_factor on the example sketch
-
- This example assumes pounds (lbs). If you prefer kilograms, change the Serial.print(" lbs"); line to kg. The
- calibration factor will be significantly different but it will be linearly related to lbs (1 lbs = 0.453592 kg).
-
- Your calibration factor may be very positive or very negative. It all depends on the setup of your scale system
- and the direction the sensors deflect from zero state
- This example code uses bogde's excellent library: https://github.com/bogde/HX711
- bogde's library is released under a GNU GENERAL PUBLIC LICENSE
- Arduino pin 2 -> HX711 CLK
- 3 -> DOUT
- 5V -> VCC
- GND -> GND
-
- Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
-
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
-
-*/
-
 #include <Wire.h> //Needed for I2C to GPS
 #include <EEPROM.h>
 
@@ -54,6 +21,7 @@ HX711 scale;
 
 float LC_scaleValue = -34200; // set this default
 long LC_offsetValue = 0;
+float LC_Force = 9999999;
 string message = "";
 int systemErrorState = 0; // 0 = no error, 1 = error
 
@@ -97,6 +65,7 @@ void setup()
   scale.begin(LC_DOUT, LC_CLK);
   scale.set_offset(LC_offsetValue);
   scale.set_scale(LC_scaleValue);
+  scale.set_raw_mode(); 
   
   Wire.begin(GPS_SDA, GPS_SCL); // SDA, SCL
 
@@ -187,7 +156,11 @@ void loop()
   long GPS_heading_105 = myGNSS.getHeading();
   float GPS_heading = GPS_heading_105 * 0.000001; // degrees
   long GPS_Seconds = myGNSS.getSecond();
-  float LC_Force = scale.get_units(3);
+
+  if(scale.is_ready()) { // only proceed if HX711 is ready to read
+  LC_Force = scale.get_units();
+  }
+  
 
   if (systemErrorState == 1)
   {
